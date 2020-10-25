047ff0af8c9598c267caeef4300ce58c6cc2c5d6
Someone-1243 <justinhye@gmail.com>
2020-10-25 15:12:36 -0400

    test

--- HCS_Workshop_2/HCS_Workshop_2.py
+++ HCS_Workshop_2/HCS_Workshop_2.py
@@ -1,1 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
 
+page = requests.get("https://www.blogto.com/restaurants/")
+
+soup = BeautifulSoup(page.content,'html.parser')
+items = soup.find_all('div', attrs={'class':'listing-thumbnail-grid-item blogto-grid-item'})
+
+for item in items:
+    print(item.p.get_text().strip())
