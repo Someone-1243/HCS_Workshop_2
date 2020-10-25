9a9fb2d6d71343197a6b7298cb7e0f9dfdfe0758
Someone-1243 <justinhye@gmail.com>
2020-10-25 17:04:01 -0400

    test

--- HCS_Workshop_2/HCS_Workshop_2.py
+++ HCS_Workshop_2/HCS_Workshop_2.py
@@ -4,7 +4,14 @@ from bs4 import BeautifulSoup
 page = requests.get("https://www.blogto.com/restaurants/")
 
 soup = BeautifulSoup(page.content,'html.parser')
-items = soup.find_all('div', attrs={'class':'listing-thumbnail-grid-item blogto-grid-item'})
+items = soup.find('div',attrs={'listing-criteria-list-wrapper'}).find_all('a', attrs={'class':'listing-criteria-link'})
 
 for item in items:
-    print(item.p.get_text().strip())
+    print(item.get_text().strip())
+    link = requests.get("https://www.blogto.com/restaurants/c/toronto/" + item.get_text().strip().lower() + '/')
+    smallSoup = BeautifulSoup(link.content, 'html.parser')
+    restaurants = smallSoup.find('div', attrs={'class':'listings-navigator-content'}).find('script',attrs={'type':'text/javascript'})
+    print(restaurants)
+    #for restaurant in restaurants:
+     #   print('check')
+      #  print(restaurant.get_text())
