cf44a256d3f0f89982f1c72c04a30bfbded68bd2
Someone-1243 <justinhye@gmail.com>
2020-10-25 22:31:31 -0400

    selenium

--- HCS_Workshop_2/HCS_Workshop_2.py
+++ HCS_Workshop_2/HCS_Workshop_2.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import re
+
+driver = webdriver.Chrome()
 
 page = requests.get("https://www.blogto.com/restaurants/")
 
@@ -7,11 +11,12 @@ soup = BeautifulSoup(page.content,'html.parser')
 items = soup.find('div',attrs={'listing-criteria-list-wrapper'}).find_all('a', attrs={'class':'listing-criteria-link'})
 
 for item in items:
-    print(item.get_text().strip())
-    link = requests.get("https://www.blogto.com/restaurants/c/toronto/" + item.get_text().strip().lower() + '/')
-    smallSoup = BeautifulSoup(link.content, 'html.parser')
-    restaurants = smallSoup.find('div', attrs={'class':'listings-navigator-content'}).find('script',attrs={'type':'text/javascript'})
-    print(restaurants)
-    #for restaurant in restaurants:
-     #   print('check')
-      #  print(restaurant.get_text())
+    print(item.get_text().strip()) 
+    driver.get("https://www.blogto.com/restaurants/c/toronto/" + item.get_text().strip().lower() + '/')
+    html = driver.page_source
+    smallSoup = BeautifulSoup(html, 'html.parser')
+    restaurants = smallSoup.find_all('a', attrs={'class':'listing-info-box-name-link'})
+    for restaurant in restaurants:
+        print(restaurant.get_text())
+    print('--------')
+    print()
